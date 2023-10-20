c335a9a778f3caef9f6ef7bb3943037d72d95061
kisyular <rellika@kisyula.com>
2023-10-20 22:27:37 +0300

    NEXTJS - IMPLEMENTING AUTHENTICATION WITH CLERK AND CONVEX

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Notion',
+	title: 'Rotion',
 	description: 'A personal note taker app build with Next JS',
 	icons: {
 		icon: [
---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+//https://neutral-mite-33.clerk.accounts.dev
+export default {
+	providers: [
+		{
+			domain: 'https://neutral-mite-33.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+}
