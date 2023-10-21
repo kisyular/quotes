bc0c070cd47b33a22004f868bbaca6f57aa66bc1
kisyular <rellika@kisyula.com>
2023-10-21 23:15:46 +0300

    NEXTJS - TRASH UI ON THE SIDEBAR USING POPOVER FROM SHADCN UI

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,18 +2,27 @@ import { cn } from '@/lib/utils'
 import {
 	ChevronsLeft,
 	MenuIcon,
+	Plus,
 	PlusCircle,
 	Search,
 	Settings,
+	Trash,
 } from 'lucide-react'
+import {
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+} from '@/components/ui/popover'
+import { toast } from 'sonner'
+
 import { usePathname, useRouter } from 'next/navigation'
 import { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
-import UserItem from './user-item'
 import { useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
+
+import UserItem from './user-item'
 import Item from './item'
-import { toast } from 'sonner'
 import DocumentList from './document-list'
 
 const Navigation = () => {
@@ -176,6 +185,24 @@ const Navigation = () => {
 				</div>
 				<div className='mt-4'>
 					<DocumentList />
+					<Item
+						onClick={handleCreate}
+						icon={Plus}
+						label='Add a page'
+					/>
+
+					<Popover>
+						<PopoverTrigger className='w-full mt-4'>
+							<Item label='Trash' icon={Trash} />
+						</PopoverTrigger>
+						<PopoverContent
+							className='p-0 w-72'
+							side={isMobile ? 'bottom' : 'right'}
+						>
+							TrashBox
+							{/* <TrashBox /> */}
+						</PopoverContent>
+					</Popover>
 				</div>
 				<div
 					onMouseDown={handleMouseDown}
