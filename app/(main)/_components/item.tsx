eaf12470513ae6bd125d0876810d29c8b404cc9b
kisyular <rellika@kisyula.com>
2023-10-21 22:26:57 +0300

    NEXTJS - CREATE NESTED DOCUMENTS ON SIDEBAR

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -11,6 +11,18 @@ import {
 import { Id } from '@/convex/_generated/dataModel'
 import { cn } from '@/lib/utils'
 import { Skeleton } from '@/components/ui/skeleton'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuSeparator,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useUser } from '@clerk/clerk-react'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import router from 'next/router'
+import { toast } from 'sonner'
 
 interface ItemProps {
 	id?: Id<'documents'>
@@ -36,7 +48,38 @@ const Item = ({
 	onExpand,
 	expanded,
 }: ItemProps) => {
+	const { user } = useUser()
+	const create = useMutation(api.documents.create)
+
+	const handleExpand = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>
+	) => {
+		event.stopPropagation()
+		onExpand?.()
+	}
+
+	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation()
+		if (!id) return
+		const promise = create({
+			title: 'Untitled',
+			parentDocument: id,
+		}).then((documentId) => {
+			if (!expanded) {
+				onExpand?.()
+			}
+			// router.push(`/documents/${documentId}`)
+		})
+
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'New note created!',
+			error: 'Failed to create a new note.',
+		})
+	}
+
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight
+
 	return (
 		<div
 			onClick={onClick}
@@ -53,7 +96,7 @@ const Item = ({
 				<div
 					role='button'
 					className='h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1'
-					onClick={() => {}}
+					onClick={handleExpand}
 				>
 					<ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
 				</div>
@@ -70,6 +113,46 @@ const Item = ({
 				<kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
 					<span className='text-xs'>⌘</span>K
 				</kbd>
+			)}
+
+			{!!id && (
+				<div className='ml-auto flex items-center gap-x-2'>
+					<DropdownMenu>
+						<DropdownMenuTrigger
+							onClick={(e) => e.stopPropagation()}
+							asChild
+						>
+							<div
+								role='button'
+								className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'
+							>
+								<MoreHorizontal className='h-4 w-4 text-muted-foreground' />
+							</div>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							className='w-60'
+							align='start'
+							side='right'
+							forceMount
+						>
+							<DropdownMenuItem onClick={() => {}}>
+								<Trash className='h-4 w-4 mr-2' />
+								Delete
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<div className='text-xs text-muted-foreground p-2'>
+								Last edited by: {user?.fullName}
+							</div>
+						</DropdownMenuContent>
+					</DropdownMenu>
+					<div
+						role='button'
+						onClick={onCreate}
+						className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'
+					>
+						<Plus className='h-4 w-4 text-muted-foreground' />
+					</div>
+				</div>
 			)}
 		</div>
 	)
