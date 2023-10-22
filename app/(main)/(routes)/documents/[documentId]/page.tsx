a6acae191813914c51471bc27615ce0e45713f98
kisyular <rellika@kisyula.com>
2023-10-23 01:46:54 +0300

    NEXTJS - IMPLEMENTED API TO EDIT DOCUMENT TITLE AND ADD ICONS TO THE DOCUMENT TITLE

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,47 @@
-const DocumentIdPage = () => {
-	return <div>DocumentIdPage</div>
+'use client'
+import Toolbar from '@/components/toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useMutation, useQuery } from 'convex/react'
+
+interface DocumentIdPageProps {
+	params: {
+		documentId: Id<'documents'>
+	}
+}
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+	const document = useQuery(api.documents.getById, {
+		documentId: params.documentId,
+	})
+
+	if (document === undefined) {
+		return (
+			<div>
+				<div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
+					<div className='space-y-4 pl-8 pt-4'>Loading</div>
+				</div>
+			</div>
+		)
+	}
+
+	if (document === null) {
+		return (
+			<div className='pb-40'>
+				<div className='h-[35vh' />
+				<div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+					Hello Hello
+					<Toolbar initialData={document} />
+				</div>
+			</div>
+		)
+	}
+	return (
+		<div className='pb-40'>
+			<div className='h-[35vh]' />
+			<div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+				<Toolbar initialData={document} />
+			</div>
+		</div>
+	)
 }
 export default DocumentIdPage
