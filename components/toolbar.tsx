95cc411ef9b227ccf21b41c72962975861a5b50b
kisyular <rellika@kisyula.com>
2023-10-23 12:40:59 +0300

    NEXTJS - IMPLEMENTED UPLOAD IMAGE TO THE EDGE STORE

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,8 @@ import { ElementRef, useRef, useState } from 'react'
 import { useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 
+import { useCoverImage } from '@/hooks/use-cover-image'
+
 import TextareaAutosize from 'react-textarea-autosize'
 
 interface ToolbarProps {
@@ -22,6 +24,8 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 
 	const update = useMutation(api.documents.update)
 	const removeIcon = useMutation(api.documents.removeIcon)
+
+	const coverImage = useCoverImage()
 
 	const enableInput = () => {
 		if (preview) return
@@ -99,7 +103,7 @@ const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 				)}
 				{!initialData.coverImage && !preview && (
 					<Button
-						onClick={() => {}}
+						onClick={coverImage.onOpen}
 						className='text-muted-foreground text-xs'
 						variant='outline'
 						size='sm'
